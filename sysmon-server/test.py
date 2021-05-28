93fcddc167f62d21bc062b8b97b8977f963a8116
Matthias <mwellstein@uni-koblenz.de>
2021-05-28 22:36:49 +0200

    Adjust test.py to new json structure

--- sysmon-server/test.py
+++ sysmon-server/test.py
@@ -1,33 +1,16 @@
 from requests import post
 
 
-def send_test(machine_name, cpu_p, mem_p, gpu_p):
-    post("http://localhost:5000/post", json={
+def send_test(machine_name, time, cpu_p, mem_p, gpu_p):
+    post("http://127.0.0.1:5000/post", json={
         "machine_name": machine_name,
-        "cpu": [
-            [
-                "2021-05-23 18:45:54",
-                cpu_p
-            ]
-        ],
-        "memory": [
-            mem_p
-        ],
-        "gpu": [
-            [
-                {
-                    "index": "0",
-                    "type": "NVIDIA GeForce GTX 1660 Ti",
-                    "uuid": "GPU-c7b2d3ec-8b29-796e-ed4b-2b295e80d270",
-                    "mem_used": 3072,
-                    "mem_total": 6144,
-                    "mem_used_percent": gpu_p
-                }
-            ]
-        ],
+        "time": [time, time, time],
+        "cpu": [cpu_p, cpu_p * 2, cpu_p + 5],
+        "memory": [mem_p, mem_p + 2, mem_p - 5],
+        "gpu": [gpu_p, gpu_p * 2, gpu_p + 7]
     })
 
 
 if __name__ == "__main__":
-    send_test("asraphael", 20, 30, 40)
-    send_test("Matthias-PC", 25, 35, 12.5)
+    send_test("asraphael", "2021-05-23 18:45:54", 20, 30, 40)
+    send_test("Matthias-PC", "2021-05-24 18:45:54", 25, 35, 12.5)
