00ac368176edb673b3c0d73e8f87b90a0365e4e8
Matthias <mwellstein@uni-koblenz.de>
2021-06-16 23:36:35 +0200

    Fixed a bug and added some comment

--- sysmon-client/src/sysmon_client/submitter.py
+++ sysmon-client/src/sysmon_client/submitter.py
@@ -5,43 +5,55 @@ from requests.adapters import HTTPAdapter
 from urllib3 import Retry
 
 
+# Seperated from continues_submit() for testing purposes.
 def submit(system_stats, server_address):
     """
     Send current system statistics to provided endpoint.
 
     :param system_stats: Current system statistics
     :param server_address: address of the server collecting system stats
-    :return: None
+    :return:
     :raises: requests.exceptions.ConnectionError
     """
-    try:
-        with requests.session() as session:
-            session.mount(
-                "http://",
-                HTTPAdapter(
-                    max_retries=Retry(
-                        total=5,
-                        connect=5,
-                        redirect=10,
-                        backoff_factor=0.5,
-                        status_forcelist=[
-                            HTTPStatus.REQUEST_TIMEOUT,  # HTTP 408
-                            HTTPStatus.CONFLICT,  # HTTP 409
-                            HTTPStatus.INTERNAL_SERVER_ERROR,  # HTTP 500
-                            HTTPStatus.BAD_GATEWAY,  # HTTP 502
-                            HTTPStatus.SERVICE_UNAVAILABLE,  # HTTP 503
-                            HTTPStatus.GATEWAY_TIMEOUT  # HTTP 504
-                        ]
-                    )
+    with requests.session() as session:
+        session.mount(
+            "http://",
+            HTTPAdapter(
+                max_retries=Retry(
+                    total=5,
+                    connect=5,
+                    redirect=10,
+                    backoff_factor=0.2,
+                    status_forcelist=[
+                        HTTPStatus.REQUEST_TIMEOUT,  # HTTP 408
+                        HTTPStatus.CONFLICT,  # HTTP 409
+                        HTTPStatus.INTERNAL_SERVER_ERROR,  # HTTP 500
+                        HTTPStatus.BAD_GATEWAY,  # HTTP 502
+                        HTTPStatus.SERVICE_UNAVAILABLE,  # HTTP 503
+                        HTTPStatus.GATEWAY_TIMEOUT  # HTTP 504
+                    ]
                 )
             )
+        )
 
-            re = session.post(server_address, json=system_stats)
-    except requests.exceptions.RequestException:
-        print(re.status_code, re.reason)
-        print(f"[{system_stats['time'][-1]}] "
+        session.post(server_address, json=system_stats)
+
+
+def continues_submit(system_stats, server_address):
+    """
+    Execute the submit function in a try except block in order for it
+    to always continue operation.
+
+    :param system_stats: Current system statistics
+    :param server_address: address of the server collecting system stats
+    :return:
+    """
+    try:
+        submit(system_stats, server_address)
+    except requests.exceptions.RequestException as exc:
+        print(f"[{system_stats['time']}] "
               f"Could not update system statistics on {system_stats['name']}.")
-    else:
-        print(f"[{system_stats['time'][-1]}] "
-              f"Updated system statistics on {system_stats['name']}.")
 
+    else:
+        print(f"[{system_stats['time']}] "
+              f"Updated system statistics on {system_stats['name']}.")
