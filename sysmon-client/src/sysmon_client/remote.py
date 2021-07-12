70bc215e6b8137a04e9fa7ff3ee0e9dd04f0d776
Matthias <mwellstein@uni-koblenz.de>
2021-07-12 17:45:10 +0200

    First remote implementation

--- sysmon-client/src/sysmon_client/remote.py
+++ sysmon-client/src/sysmon_client/remote.py
@@ -1,7 +1,10 @@
 """
 Establish remote connection and issue commands over ssh
 """
+from pathlib import Path
+from typing import List
 import spur
+
 
 # TODO: Evaluate if i can provide an easier interface for our use cases
 # Otherwise integrate spur into the functions that would call this
@@ -10,35 +13,43 @@ import spur
 
 
 class Remote:
-    def __init__(self):
-        self.hostname = ""
-        self.user_name = ""
-        self.password = ""
-        self.key_file = ""  # Maybe Path
-        self.port = 22
+    def __init__(self,
+                 hostname: str,
+                 username: str,
+                 password: str = None,
+                 key_file: str or Path = None,
+                 port: int = 22):
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        if key_file:
+            key_file = Path(key_file).expanduser().resolve()
+            if key_file.is_file():
+                # Spur does not work with Path input
+                self.key_file = str(key_file)
+            else:
+                raise FileNotFoundError(f"No keyfile found: {key_file}")
+        else:
+            # This way an input of "" is going back to None
+            # Default of spur for no key is None
+            self.key_file = None
+        self.port = port
 
-    # This was a small test of spur, ignore for now
-    @staticmethod
-    def connect_with_ssh(hostname: str,
-                         username: str,
-                         password: str = None,
-                         key_file: str = None,
-                         port: int = None):
-        shell = spur.SshShell(hostname=hostname,
-                              username=username,
-                              password=password,
-                              private_key_file=key_file,
-                              port=port)
-        with shell:
-            result = shell.run(["pwd"])
+        # Enables arbitrary execution on remote.
+        self.local_shell = spur.LocalShell()
+        self.shell = spur.SshShell(hostname=self.hostname,
+                                   username=self.username,
+                                   password=self.password,
+                                   private_key_file=self.key_file,
+                                   port=self.port)
 
-            return result.output
+        self.test_connection()
+
+    def test_connection(self):
+        """Run simple test command, errors out if authentication fails."""
+        with self.shell as shell:
+            shell.run(["pwd"])
 
 
 if __name__ == '__main__':
-    host = input("Host: ")
-    user = input("User: ")
-    passw = input("Password: ")
-    test = Remote.connect_with_ssh(host, user, passw)
-
-    print(test)
+    pass
