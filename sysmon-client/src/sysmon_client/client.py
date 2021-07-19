4a0d7910c2a7905b2b8acafdd80cbbbba42831e6
Matthias <mwellstein@uni-koblenz.de>
2021-07-19 23:27:36 +0200

    Make Client a class to import and to get it in line with all other modules.

--- sysmon-client/src/sysmon_client/client.py
+++ sysmon-client/src/sysmon_client/client.py
@@ -7,21 +7,26 @@ from collector import Collector
 import submitter
 
 
-def start_reporting(name: str, server_address: str, interval: int = 60):
-    """
-    Report's continuously the status of this machine to the sysmon_server.
+class Client:
+    def __init__(self, name, server_address, report_interval: int = 60):
+        """
 
-    :param name: The name of this client, that will be displayed on the server
-    :param server_address: Address, with endpoint, of the sysmon_server
-    :param interval: Number of seconds between updates
-    :return:
-    """
-    while True:
-        system_stats = Collector(name, interval)
-        submitter.continues_submit(system_stats.json, server_address)
-        time.sleep(interval)
+        :param name: The name of this client, that will be displayed on the server.
+        :param server_address: Address of the sysmon_server.
+        :param report_interval: Number of seconds between sending a report.
+        """
+        self.name = name
+        self.server_address = server_address
+        self.report_interval = report_interval
+
+    def start_reporting(self):
+        """Report's continuously the status of this machine to the sysmon_server."""
+        while True:
+            system_stats = Collector(self.name, self.report_interval)
+            submitter.continues_submit(system_stats.json, self.server_address)
+            time.sleep(self.report_interval)
 
 
 if __name__ == "__main__":
-    start_reporting("Test", "http://127.0.0.1:5000", 5)
+    Client("Test", "http://127.0.0.1:5000", 5).start_reporting()
     # submitter.request_deletion({"name": "NoGpu"}, "http://127.0.0.1:5000")
